435ef951ea087a1c5fccab451ec174c1a5026e2a
Hubert <konczito33@gmail.com>
2021-03-21 18:58:43 +0100

    1.1

---
+++ script.js
@@ -0,0 +1,48 @@
+(function innit() {
+
+    const refreshBtn = document.querySelector('.quote__button')
+    refreshBtn.addEventListener('click', getQuote)
+
+    const city = document.querySelector('.main__city')
+    const hour = document.querySelector('.main__hour')
+
+
+    function getQuote() {
+        let random = Math.floor(Math.random() * 44)
+        const quoteTxt = document.querySelector('.quote__text')
+        const quoteAut = document.querySelector('.quote__author')
+
+        const config = {
+            headers: {
+                Accept: 'application/json'
+            },
+        }
+
+        fetch('http://quotes.stormconsultancy.co.uk/quotes.json', config).then((res) => res.json().then((data) => {
+            quoteTxt.textContent = data[random].quote, quoteAut.textContent = data[random].author
+        }))
+    }
+
+    getQuote()
+
+    function getLocation() {
+        const config = {
+            headers: {
+                Accept: 'application/json'
+            },
+        }
+        fetch('http://ip-api.com/json/', config).then((res) => res.json().then((data) =>  city.textContent = `in ${data.city}`))
+    }
+
+    getLocation()
+
+    function setTime(){
+        const date  = new Date
+        let hours = date.getHours()
+        let minutes = date.getMinutes()
+        hour.textContent = `${hours}:${minutes}`
+    }
+
+    setInterval(setTime, 1000)
+
+})()
