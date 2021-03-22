3add2b0ed05d8c61fcca71ae4bc5a1cc788b6245
Hubert <konczito33@gmail.com>
2021-03-22 01:21:11 +0100

    1.2

--- script.js
+++ script.js
@@ -5,6 +5,10 @@
 
     const city = document.querySelector('.main__city')
     const hour = document.querySelector('.main__hour')
+    const bg = document.querySelector('.bg')
+    const sun = document.querySelector('.sun')
+    const moon = document.querySelector('.moon')
+    const greeting = document.querySelector('.evening-morning')
 
 
     function getQuote() {
@@ -31,18 +35,83 @@
                 Accept: 'application/json'
             },
         }
-        fetch('http://ip-api.com/json/', config).then((res) => res.json().then((data) =>  city.textContent = `in ${data.city}`))
+        fetch('http://ip-api.com/json/', config).then((res) => res.json().then((data) => {
+            city.textContent = `in ${data.city}, ${data.country}`, moreInfo(data)
+        }))
     }
 
     getLocation()
 
-    function setTime(){
-        const date  = new Date
+    function setTime() {
+        const date = new Date
         let hours = date.getHours()
         let minutes = date.getMinutes()
-        hour.textContent = `${hours}:${minutes}`
+
+        hour.textContent = `${hours}:${minutes < 10 ? `0${minutes}` : minutes}`
+    }
+
+    function changeBg() {
+        const date = new Date
+        let hours = date.getHours()
+
+        if (hours > 22 || hours < 6) {
+            bg.style.backgroundImage = 'url(assets/dark-img.jpeg)'
+            moon.style.fontSize = '25px'
+            sun.style.fontSize = '0px'
+            greeting.textContent = 'evening'
+
+
+        } else {
+            bg.style.backgroundImage = 'url(assets/light-img.jpeg)'
+            moon.style.fontSize = '0px'
+            sun.style.fontSize = '25px'
+            greeting.textContent = 'morning'
+        }
     }
 
     setInterval(setTime, 1000)
+
+    changeBg()
+    setInterval(changeBg, 10000)
+
+
+    const moreBtn = document.querySelector('.more-btn')
+
+    moreBtn.addEventListener('click', () => {
+        const container = document.querySelector('.container')
+        const infoContainer = document.querySelector('.more-info-container ')
+        container.classList.toggle('small')
+        infoContainer.classList.toggle('active')
+        if (container.classList.contains('small')) {
+            moreBtn.textContent = 'less'
+        } else {
+            moreBtn.textContent = 'more'
+        }
+    })
+
+    function moreInfo(data) {
+        const timezone = document.getElementById('timezone')
+        const dayOfWeek = document.getElementById('dayOfWeek')
+        const dayOfYear = document.getElementById('dayOfYear')
+        const weekNum = document.getElementById('weekNum')
+        const date = new Date
+        const days = ['', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+        const dayOfYearNum = date =>
+            Math.floor((date - new Date(date.getFullYear(), 0, 0)) / 1000 / 60 / 60 / 24);
+
+        Date.prototype.getWeekNumber = function () {
+            let d = new Date(Date.UTC(this.getFullYear(), this.getMonth(), this.getDate()));
+            let dayNum = d.getUTCDay() || 7;
+            d.setUTCDate(d.getUTCDate() + 4 - dayNum);
+            let yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+            return Math.ceil((((d - yearStart) / 86400000) + 1) / 7)
+        };
+
+        timezone.textContent = data.timezone
+        dayOfWeek.textContent = days[date.getDay()]
+        dayOfYear.textContent = dayOfYearNum(date)
+        weekNum.textContent = date.getWeekNumber()
+    }
+
 
 })()
